a6a76b55b6eff05753d7320f1edc9c4b8de5edf2
Aabed20 <aabedkhanpathan699@gmail.com>
2025-12-25 16:08:52 +0530

    25-Dec-2023 14:15:162023-12-25 14:15:16 +0530

---
+++ Javascript/index.js
@@ -0,0 +1,27 @@
+// let a = 25;
+// console.log( typeof a);
+// a = "prasad";
+// console.log(typeof a);
+// a = [1, 2];
+// console.log(typeof a);
+// a = {};
+// console.log(typeof a);
+// function x () {}
+// console.log(typeof a);
+let num = 25.4168;
+console.log(typeof num);
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));
+num = 25;
+console.log(num.toString(2));
+
+// math methods
+console.log(Math.sqrt(16)); //4 square root dega
+console.log(Math.cbrt(27)); //3 cube root dega
+console.log(Math.pow(2,3)); //8 power of 2*2*2
+console.log(Math.abs(-10)); //10 change given negative number to positive
+console.log(Math.max(5,3,12,25)); //25 gives max value
+console.log(Math.min(2,3,1,0)); //0 gives min value
+console.log(Math.ceil(2.1)); //3 gives the nearly great value
+console.log(Math.floor(3.2)); //3 gives the nearly small value
+console.log(Math.round(2.6)); //3 gives nearest great value if .5 or more than 5 otherwise remove decimal nuumber 
